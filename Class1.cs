5ae56054c98ed9a3dec1331d1f9a5d7d88ed3d97
Sammy Wong <sammywong528@outlook.com>
2023-09-12 15:24:17 +0200

    Create class Person and new objects

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalRegister
+{
+    internal class Personal
+    {
+        string name;
+        int salary;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,30 @@
-﻿namespace PersonalRegister
+﻿using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace PersonalRegister
 {
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Salary { get; set; }
+        public Person(string firstName, string lastName, int salary) 
+        {
+           FirstName = firstName;
+            LastName = lastName;
+            Salary = salary;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            Person peterS = new Person("Peter", "Svensson", 25000);
+            Person yvonneA = new Person("Yvonne", "Andersson", 30000);
+            Person martenB = new Person("Mårten", "Beck", 27000);
+
+
         }
     }
 }
